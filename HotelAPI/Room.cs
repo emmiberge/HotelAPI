55515dc8bc98975d2acb3ec954ac3ea30f27481e
emmiberge <emmieberger@sogeti.se>
2024-08-05 12:46:44 +0200

    room data type

---
+++ HotelAPI/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Room
+{
+	public readonly int ID;
+	public readonly int AmountOfBeds;
+	public readonly double PricePerNight;
+
+    public Room(int iD, int amountOfBeds, double pricePerNight)
+    {
+        ID = iD;
+        AmountOfBeds = amountOfBeds;
+        PricePerNight = pricePerNight;
+    }
+}
---
+++ HotelAPI/TestData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class TestData
+{
+	public static Room[] GetTestData()
+	{
+		Room[] rooms = { new Room(1, 2, 300.0)};
+		return rooms;
+	}
+}
