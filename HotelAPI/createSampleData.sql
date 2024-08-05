8ec5d0c0f56c5347be698aa57dd46c498cbaccdf
emmiberge <emmieberger@sogeti.se>
2024-08-05 13:05:53 +0200

    sql dummy data

---
+++ HotelAPI/createSampleData.sql
@@ -0,0 +1,13 @@
+﻿DROP TABLE RoomsTestTable;
+
+CREATE TABLE RoomsTestTable (
+	ID int PRIMARY KEY,
+	AmountOfBeds int NOT NULL,
+	PricePerNight decimal(10,2)
+);
+
+INSERT INTO RoomsTestTable VALUES (1,1,150.0);
+INSERT INTO RoomsTestTable VALUES (2,2,300.0);
+INSERT INTO RoomsTestTable VALUES (3,2,350.0);
+
+SELECT * FROM RoomsTestTable;
