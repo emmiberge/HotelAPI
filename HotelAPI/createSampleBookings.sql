411b1e5cf6614ea55fa3d499799265b6725e4ea0
emmiberge <emmieberger@sogeti.se>
2024-08-06 16:54:01 +0200

    script to create and insert dummy data for bookings

---
+++ HotelAPI/createSampleBookings.sql
@@ -0,0 +1,22 @@
+﻿DROP TABLE BookingsTestTable;
+
+CREATE TABLE BookingsTestTable (
+	ID int PRIMARY KEY,
+	RoomID int,
+	CustomerID int,
+	CheckInDate Date,
+	CheckOutDate Date
+);
+
+INSERT INTO BookingsTestTable VALUES (1,1,100,'2024-03-03','2024-03-05');
+INSERT INTO BookingsTestTable VALUES (2,1,200,'2024-03-05','2024-03-08');
+INSERT INTO BookingsTestTable VALUES (3,2,300,'2024-03-03','2024-03-06');
+INSERT INTO BookingsTestTable VALUES (4,2,400,'2024-03-06','2024-03-07');
+INSERT INTO BookingsTestTable VALUES (5,3,500,'2024-03-07','2024-03-10');
+INSERT INTO BookingsTestTable VALUES (6,8,600,'2024-03-02','2024-03-12');
+INSERT INTO BookingsTestTable VALUES (7,9,700,'2024-03-04','2024-03-05');
+INSERT INTO BookingsTestTable VALUES (8,9,800,'2024-03-05','2024-03-06');
+INSERT INTO BookingsTestTable VALUES (9,9,900,'2024-03-06','2024-03-08');
+INSERT INTO BookingsTestTable VALUES (10,10,1000,'2024-03-08','2024-03-09');
+
+SELECT * FROM BookingsTestTable;
--- HotelAPI/createSampleRooms.sql
+++ HotelAPI/createSampleRooms.sql
@@ -9,5 +9,6 @@ CREATE TABLE RoomsTestTable (
 INSERT INTO RoomsTestTable VALUES (1,1,150.0);
 INSERT INTO RoomsTestTable VALUES (2,2,300.0);
 INSERT INTO RoomsTestTable VALUES (3,2,350.0);
+INSERT INTO RoomsTestTable VALUES (4,4,650.0);
 
 SELECT * FROM RoomsTestTable;
